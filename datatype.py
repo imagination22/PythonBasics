39da98cb7357c86977af183bc49f2ac8e146d9c1
shubham <barmanshubham1822@gamil.com>
2024-05-26 21:21:35 +0530

    add datatypes

---
+++ datatype.py
@@ -0,0 +1,36 @@
+a = 2
+b = 2.0
+c = 1j
+d = 2+1J
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+e = 'shubham with single quotes'
+f = "shubham with double quotes"
+g = '''Shubham is 
+in next line'''
+h = """shubham is in
+ next line with 
+ double quotes"""
+k = ("shubham","basudev","barman")
+l = ["shubham","basudev","barman"]
+m= {"shubham","basudev","barman"}
+n = {1:"shubham" , 2: "barman"}
+
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+
+o= b"shubham"
+print(o)
+print(type(o))
+
+p = True
+print(type(p))
